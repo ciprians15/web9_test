c8854e1e50e7c4b7341b9f6f616d73c94ce46106
ciprian <simionov.ciprian@gmail.com>
2017-03-29 16:54:49 +0000

    implemented update method for Article model

--- blog/UI/models/Article.js
+++ blog/UI/models/Article.js
@@ -14,15 +14,6 @@ Article.prototype.update = function(data)
     //here we save data to server.
     //do an Ajax request or save it in local storage.
 };
-function Article(options)
-{
-    this.id=options.id;
-    this.title=options.title;
-    this.category=options.category || "default topic";
-    this.content=options.content;
-    this.date=options.date || new Date();
-}
-Article.prototype.save=function(articleData){};
-Article.prototype.update=function(data);
 
 //git modify
+// this code is added only on feature_update_article branch 
